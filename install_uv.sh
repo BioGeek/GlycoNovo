eeccdb70e0a069fa19c9fda3105146fc5ecee2ca
Jeroen Van Goey <j.vangoey@instadeep.com>
2025-09-30 10:59:48 +0200

    Add torch geometric

--- install_uv.sh
+++ install_uv.sh
@@ -1,6 +1,7 @@
 uv pip install torch==2.2.1 torchvision==0.17.1 torchaudio==2.2.1 -f https://download.pytorch.org/whl/cu121 \
     pyg-lib torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-2.4.0+cu121.html \
     dgl dglgo -f https://data.dgl.ai/wheels/torch-2.1/cu121/repo.html \
+    torch-geometric \
     glypy \
     fairseq \
     scikit-learn \
